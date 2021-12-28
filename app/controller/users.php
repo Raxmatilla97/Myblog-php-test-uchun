676a6161640a17377fa1436f978b58d46f1a64cb
Raxmatilla <wi.fi.xor@gmail.com>
2021-12-28 23:05:06 +0500

    Users controllerda ro'yxatdan o'tqazish funksiyasini tugatdim

--- app/controller/users.php
+++ app/controller/users.php
@@ -13,26 +13,32 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST'){
     $passF = trim($_POST['password-first']);
     $passS = trim($_POST['password-second']);
 
+
+
     if ($user === '' || $email === '' || $passF === ''){
     $errMsg = "Formada maydonchalar to'ldirilmagan!";
     }elseif (mb_strlen($user, 'UTF8') < 2){
         $errMsg = "Login 2-x simvoldan kotta bo'lishi kere!";
     }elseif ($passF !== $passS){
         $errMsg = "Tasdiqlash paroli o'xshash emas, parolni tog'ri yozing.";
-    }elseif (selectOne('users', ['email' => '$email'])){
-        echo "Email mavjud!";
-    }
-    else {
-        $pass = password_hash($passF, PASSWORD_DEFAULT);
-        $post = [
-            'admin' => $admin,
-            'username' => $user,
-            'email' => $email,
-            'password' => $pass
-        ];
-        //    $id = insert('users', $post);
-        $isSubmit = true;
-        $id = insert('users', $post);
+    }else {
+        $existence = selectOne('users', ['email' => $email]);
+        if ($existence['email'] === $email){
+            $errMsg = $email . " = nomli email foydalanuvchisi ro'yxatdan o'tgan!";
+        }else{
+            $pass = password_hash($passF, PASSWORD_DEFAULT);
+            $post = [
+                'admin' => $admin,
+                'username' => $user,
+                'email' => $email,
+                'password' => $pass
+            ];
+            //    $id = insert('users', $post);
+            $isSubmit = true;
+            $id = insert('users', $post);
+            $errMsg = "Foydalanuvchi <strong> $user </strong> ro'yxatdan o'tqazildi!";
+        }
+
     }
 
 
