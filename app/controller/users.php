7bf6a1d73ef0237b0370813259bb0bada34fbb2b
Raxmatilla <wi.fi.xor@gmail.com>
2021-12-28 22:03:50 +0500

    Users controllerni tog'riladim

--- app/controller/users.php
+++ app/controller/users.php
@@ -1,22 +1,46 @@
 <?php
 include_once "app/database/db.php";
 
-if (isset($_POST['button-reg'])){
+$isSubmit = false;
+$errMsg = '';
+
+
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST'){
     $admin = 0;
-    $user = $_POST['username'];
-    $email = $_POST['email'];
-    $pass = password_hash($_POST['password-second'], PASSWORD_DEFAULT);
-    
-    $post = [
-        'admin' => $admin,
-        'username' => $user,
-        'email' => $email,
-        'password' => $pass
-    ];
+    $user = trim($_POST['username']);
+    $email = trim($_POST['email']);
+    $passF = trim($_POST['password-first']);
+    $passS = trim($_POST['password-second']);
 
-  $id = insert('users', $post);
-  $last_row = selectOne('users', ['id' => $id]);
-  
-  test($last_row);
+    if ($user === '' || $email === '' || $passF === ''){
+    $errMsg = "Formada maydonchalar to'ldirilmagan!";
+    }elseif (mb_strlen($user, 'UTF8') < 2){
+        $errMsg = "Login 2-x simvoldan kotta bo'lishi kere!";
+    }elseif ($passF !== $passS){
+        $errMsg = "Tasdiqlash paroli o'xshash emas, parolni tog'ri yozing.";
+    }elseif (selectOne('users', ['email' => '$email'])){
+        echo "Email mavjud!";
+    }
+    else {
+        $pass = password_hash($passF, PASSWORD_DEFAULT);
+        $post = [
+            'admin' => $admin,
+            'username' => $user,
+            'email' => $email,
+            'password' => $pass
+        ];
+        //    $id = insert('users', $post);
+        $isSubmit = true;
+        $id = insert('users', $post);
+    }
 
+
+}else{
+    echo 'GET';
+    $user = '';
+    $email = '';
 }
+
+
+
